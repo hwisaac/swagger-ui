3265a15aa652f3a513c7438f7dae092dc93b705d
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-08-22 14:26:28 +0900

    translators

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://translators-bucket.s3.ap-northeast-2.amazonaws.com/swagger/translators.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
